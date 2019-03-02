e21b06edb2f395e9c12c84480feef01683157249
kush-daga <kushdaga@hotmail.com>
2019-03-02 13:24:17 +0530

    added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-module.exports = (app) => {
+module.exports = app => {
   // API routes
-  fs.readdirSync(__dirname + '/api/').forEach((file) => {
-    require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
+  fs.readdirSync(__dirname + "/api/").forEach(file => {
+    require(`./api/${file.substr(0, file.indexOf("."))}`)(app);
   });
 };
